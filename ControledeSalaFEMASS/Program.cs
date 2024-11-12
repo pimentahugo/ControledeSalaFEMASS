3fc46bf78716b8e554f640ff30fc47f07c49c99d
Hugo <hugopimenta_marinho_machado@hotmail.com>
2024-11-11 23:43:46 -0300

    feat: liberando swagger para front end

--- ControledeSalaFEMASS/Program.cs
+++ ControledeSalaFEMASS/Program.cs
@@ -26,11 +26,11 @@ ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
